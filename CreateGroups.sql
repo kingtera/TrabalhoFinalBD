c2694c83cb3331ba4579202db31dcad313356c7a
EduTera <eduardoteranisi@gmail.com>
2024-10-30 23:02:37 -0300

    create: users

--- CreateGroups.sql
+++ CreateGroups.sql
@@ -2,3 +2,9 @@
 
 CREATE ROLE desenvolvedor;
 CREATE ROLE gerente_projeto;
+
+CREATE USER dev WITH PASSWORD 'senha';
+CREATE USER manager_dev WITH PASSWORD 'senha';
+
+GRANT desenvolvedor TO dev;
+GRANT gerente_projeto TO manager_dev;
